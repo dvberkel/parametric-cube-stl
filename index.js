53cb06a7388e18274dd5a33ae9e3e4ad759d0156
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-02-02 09:47:05 +0100

    Convert a number to scientific notation

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 #! /usr/bin/env node
 
-var cube = require('dot').process({ path: './templates' });
+var dot = require('dot');
+var cube = dot.process({ path: './templates' });
 
-var result = cube.solid({ a: '1.000000e+00' });
+function digits(n) {
+    return Math.floor(Math.log(n)/Math.log(10) + 1);
+}
+
+function padRight(word, pad, length) {
+    var copy = word.slice(0);
+    while (copy.length < length) {
+        copy += pad;
+    }
+    return copy.slice(0, length);
+}
+
+function toScientificNotation(description) {
+    var units = parseInt(description);
+    var d = digits(units) - 1;
+    var e = d < 10 ? '0' + d.toString() : d.toString();
+    var n = description[0] + '.' + description.slice(1);
+    var m = padRight(n, '0', 8);
+    console.log(m, e);
+
+    return m + 'e+' + e;
+}
+
+var result = cube.solid({ a: toScientificNotation('123') });
 
 console.log(result);
