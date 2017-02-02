7fe5778f4a4c2589059b532731c1babca620968e
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-02-02 09:10:08 +0100

    Rename variables and templates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var dots = require('dot').process({ path: './templates' });
+var cube = require('dot').process({ path: './templates' });
 
-var result = dots.cube({ a: '1.000000e+00' });
+var result = cube.solid({ a: '1.000000e+00' });
 
 console.log(result);
