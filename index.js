ae2af3a24e6eefd4c0d35ca901f17f203e4a4e40
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-02-02 09:50:43 +0100

    Create a science library

--- index.js
+++ index.js
@@ -2,29 +2,7 @@
 
 var dot = require('dot');
 var cube = dot.process({ path: './templates' });
-
-function digits(n) {
-    return Math.floor(Math.log(n)/Math.log(10) + 1);
-}
-
-function padRight(word, pad, length) {
-    var copy = word.slice(0);
-    while (copy.length < length) {
-        copy += pad;
-    }
-    return copy.slice(0, length);
-}
-
-function toScientificNotation(description) {
-    var units = parseInt(description);
-    var d = digits(units) - 1;
-    var e = d < 10 ? '0' + d.toString() : d.toString();
-    var n = description[0] + '.' + description.slice(1);
-    var m = padRight(n, '0', 8);
-    console.log(m, e);
-
-    return m + 'e+' + e;
-}
+var toScientificNotation = require('./lib/science');
 
 var result = cube.solid({ a: toScientificNotation('123') });
 
