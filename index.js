8f52067cd628ec81c2c6c740db255fba3fed1660
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-02-02 21:00:33 +0100

    Create a hollow cube template

--- index.js
+++ index.js
@@ -5,11 +5,28 @@ var cube = dot.process({ path: './templates' });
 var toScientificNotation = require('./lib/science');
 
 var figure = 'solid';
+var units = {};
 var data = {};
 switch (process.argv.length) {
 case 3:
     figure = 'solid';
-    data.a = parseInt(process.argv[2]);
+    units.a = parseInt(process.argv[2]);
+    datat = {
+        a: toScientificNotation(units.a)
+    };
+    break;
+case 4:
+    figure = 'hollow';
+    units.a = parseInt(process.argv[2]);
+    units.b = parseInt(process.argv[3]);
+    if (units.b >= units.a) {
+        throw new Error('b should be smaller than a');
+    }
+    data = {
+        a: toScientificNotation(units.a),
+        b: toScientificNotation(units.b),
+        d: toScientificNotation(units.a - units.b)
+    };
     break;
 default:
     figure = 'solid';
@@ -17,6 +34,6 @@ default:
     break;
 }
 
-var result = cube[figure]({ a: toScientificNotation(data.a) });
+var result = cube[figure](data);
 
 console.log(result);
