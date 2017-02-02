10050759b4ae545cc56a345ab00bd13f85834bd5
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-02-02 21:02:42 +0100

    Refactor data

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ var cube = dot.process({ path: './templates' });
 var toScientificNotation = require('./lib/science');
 
 var figure = 'solid';
-var units = {};
-var data = {};
+var units = { a: 0, b: 0, c: 0, d: 0, e: 0 };
 switch (process.argv.length) {
 case 3:
     figure = 'solid';
     units.a = parseInt(process.argv[2]);
-    datat = {
-        a: toScientificNotation(units.a)
-    };
     break;
 case 4:
     figure = 'hollow';
@@ -22,17 +18,18 @@ case 4:
     if (units.b >= units.a) {
         throw new Error('b should be smaller than a');
     }
-    data = {
-        a: toScientificNotation(units.a),
-        b: toScientificNotation(units.b),
-        d: toScientificNotation(units.a - units.b)
-    };
     break;
 default:
     figure = 'solid';
     data.a = 1;
     break;
 }
+
+var data = {
+    a: toScientificNotation(units.a),
+    b: toScientificNotation(units.b),
+    d: toScientificNotation(units.a - units.b)
+};
 
 var result = cube[figure](data);
 
