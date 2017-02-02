4b7e83115b90d86a798720d83ffdd20741104a0e
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-02-02 16:05:30 +0100

    Accept a number

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var dot = require('dot');
 var cube = dot.process({ path: './templates' });
 var toScientificNotation = require('./lib/science');
 
-var result = cube.solid({ a: toScientificNotation('123') });
+var result = cube.solid({ a: toScientificNotation(123) });
 
 console.log(result);
