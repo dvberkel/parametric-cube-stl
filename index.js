ff8e9c100e1c25e4af894e0de41a3e7e32b81a30
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-02-02 16:13:55 +0100

    Accept an argument

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ var dot = require('dot');
 var cube = dot.process({ path: './templates' });
 var toScientificNotation = require('./lib/science');
 
-var result = cube.solid({ a: toScientificNotation(123) });
+var figure = 'solid';
+var data = {};
+switch (process.argv.length) {
+case 3:
+    figure = 'solid';
+    data.a = parseInt(process.argv[2]);
+    break;
+default:
+    figure = 'solid';
+    data.a = 1;
+    break;
+}
+
+var result = cube[figure]({ a: toScientificNotation(data.a) });
 
 console.log(result);
