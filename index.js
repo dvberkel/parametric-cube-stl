66de3c679023f819a2589f038d51e51eb91c1c79
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-02-04 09:58:15 +0100

    Abstract argument parsing

--- index.js
+++ index.js
@@ -4,64 +4,9 @@ var dot = require('dot');
 dot.templateSettings.strip = false;
 
 var cube = dot.process({ path: './templates' });
-var toScientificNotation = require('./lib/science');
+var parse = require('./lib/arguments');
 
-var figure = 'solid';
-var units = { a: 0, b: 0, c: 0, d: 0, e: 0 };
-switch (process.argv.length) {
-case 3:
-    figure = 'solid';
-    units.a = parseInt(process.argv[2]);
-    break;
-case 4:
-    figure = 'hollow';
-    units.a = parseInt(process.argv[2]);
-    units.b = parseInt(process.argv[3]);
-    if (units.b >= units.a) {
-        throw new Error('b should be smaller than a');
-    }
-    if (units.b <= units.a/2) {
-        throw new Error('b should be bigger than a/2');
-    }
-    break;
-case 5:
-    console.log('windowed');
-    figure = 'windowed';
-    units.a = parseInt(process.argv[2]);
-    units.b = parseInt(process.argv[3]);
-    units.c = parseInt(process.argv[4]);
-    if (units.b >= units.a) {
-        throw new Error('b should be smaller than a');
-    }
-    if (units.b <= units.a/2) {
-        throw new Error('b should be bigger than a/2');
-    }
-    if (units.c >= units.a) {
-        throw new Error('c should be smaller than a');
-    }
-    if (units.b <= units.a/2) {
-        throw new Error('c should be bigger than a/2');
-    }
-    if (units.c >= units.b) {
-        throw new Error('c should be smaller than b');
-    }
-    break;
-default:
-    figure = 'solid';
-    units.a = 1;
-    break;
-}
-
-var data = {
-    figure: figure,
-    measure: {
-        a: toScientificNotation(units.a),
-        b: toScientificNotation(units.b),
-        c: toScientificNotation(units.c),
-        d: toScientificNotation(units.a - units.b),
-        e: toScientificNotation(units.a - units.c)
-    }
-};
+var data = parse(process.argv);
 
 var result = cube[data.figure](data.measure);
 
